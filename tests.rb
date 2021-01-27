a01f73dc894eb87997ba103a6d5efffa8123765e
Jason Easterly <jason.easterly11@gmail.com>
2021-01-27 17:01:43 +0000

    Automatically backed up by Learn

---
+++ tests.rb
@@ -0,0 +1,17 @@
+
+def output_array_element array
+counter = 0
+
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
+end
+
+pets = ["Dog", "Cat", "Fish", "Bird", "Hamster"]
+
+
+output_array_element([1, 2, 3])
+
+
+
